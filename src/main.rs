de77eb7b947510df965c0925b983fb297838face
wiKapo <71190167+wiKapo@users.noreply.github.com>
2023-04-10 17:52:04 +0200

    Many changes.

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,104 @@ pub const PLAYER_SIZE: f32 = 64.0;
 pub const NUMBER_OF_ENEMIES: usize = 4;
 pub const ENEMY_SPEED: f32 = 200.0;
 pub const ENEMY_SIZE: f32 = 64.0;
+pub const NUMBER_OF_CANDIES: usize = 10;
+pub const CANDY_SIZE: f32 = 32.0;
+pub const CANDY_SPAWN_TIME: f32 = 1.0;
+pub const ENEMY_SPAWN_TIME: f32 = 5.0;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins)
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_resource::<Score>()
+        .init_resource::<CandySpawnTimer>()
+        .init_resource::<EnemySpawnTimer>()
+        .init_resource::<CodeTimer>()
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_enemies)
+        .add_startup_system(spawn_candies)
         .add_system(player_movement)
         .add_system(enemy_movement)
         .add_system(confine_player_movement)
         .add_system(update_enemy_direction)
         .add_system(confine_enemy_movement)
+        .add_system(enemy_hit_player)
+        .add_system(player_hit_candy)
+        .add_system(update_score)
+        .add_system(tick_candy_spawn_timer)
+        .add_system(spawn_candies_over_time)
+        .add_system(tick_enemy_spawn_timer)
+        .add_system(spawn_enemies_over_time)
         .run();
 }
 
 #[derive(Component)]
-pub struct Player {}
+pub struct Player {
+    pub old_sprites: bool,
+}
 
 #[derive(Component)]
 pub struct Enemy {
     pub direction: Vec2,
+}
 
+#[derive(Component)]
+pub struct Candy {}
+
+#[derive(Resource)]
+pub struct Score {
+    pub value: u32,
+}
+
+//noinspection RsTraitImplementation
+impl Default for Score {
+    fn default() -> Score {
+        Score { value: 0 }
+    }
+}
+
+#[derive(Resource)]
+pub struct CandySpawnTimer {
+    pub timer: Timer,
+}
+
+//noinspection RsTraitImplementation
+impl Default for CandySpawnTimer {
+    fn default() -> CandySpawnTimer {
+        CandySpawnTimer {
+            timer: Timer::from_seconds(CANDY_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+
+//noinspection RsTraitImplementation
+impl Default for EnemySpawnTimer {
+    fn default() -> EnemySpawnTimer {
+        EnemySpawnTimer {
+            timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct CodeTimer {
+    pub count: u32,
+    pub timer: u32,
+}
+
+//noinspection RsTraitImplementation
+impl Default for CodeTimer {
+    fn default() -> CodeTimer {
+        CodeTimer {
+            count: 0,
+            timer: 0,
+        }
+    }
 }
 
 pub fn spawn_player(
@@ -45,7 +122,7 @@ pub fn spawn_player(
                 texture: asset_server.load("sprites/player.png"),
                 ..default()
             },
-            Player {},
+            Player { old_sprites: false },
         ));
 }
 
@@ -82,6 +159,28 @@ pub fn spawn_enemies(
             Enemy {
                 direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
             }
+        ));
+    }
+}
+
+pub fn spawn_candies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_CANDIES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/candy.png"),
+                ..default()
+            },
+            Candy {}
         ));
     }
 }
@@ -218,3 +317,129 @@ pub fn confine_enemy_movement(
         transform.translation = translation;
     }
 }
+
+pub fn enemy_hit_player(
+    mut commands: Commands,
+    mut player_query: Query<(Entity, &Transform), With<Player>>,
+    enemy_query: Query<&Transform, With<Enemy>>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+) {
+    if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
+        for enemy_transform in enemy_query.iter() {
+            let distance = player_transform
+                .translation
+                .distance(enemy_transform.translation);
+            let player_radius = PLAYER_SIZE / 2.0;
+            let enemy_radius = ENEMY_SIZE / 2.0;
+            if distance < player_radius + enemy_radius {
+                println!("Enemy hit player! Game Over!");
+                let sound_effect = asset_server.load("audio/ded.ogg");
+                audio.play(sound_effect);
+                commands.entity(player_entity).despawn();
+            }
+        }
+    }
+}
+
+pub fn player_hit_candy(
+    mut commands: Commands,
+    candy_query: Query<(Entity, &Transform), With<Candy>>,
+    player_query: Query<&Transform, With<Player>>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+    mut score: ResMut<Score>,
+) {
+    if let Ok(player_transform) = player_query.get_single() {
+        for (candy_entity, candy_transform) in candy_query.iter() {
+            let distance = player_transform
+                .translation
+                .distance(candy_transform.translation);
+            if distance < PLAYER_SIZE / 2.0 + CANDY_SIZE / 2.0 {
+                println!("Player ate candy!");
+                score.value += 1;
+                let sound_effect = asset_server.load("audio/munch.ogg");
+                audio.play(sound_effect);
+                commands.entity(candy_entity).despawn();
+            }
+        }
+    }
+}
+
+pub fn update_score(score: Res<Score>) {
+    if score.is_changed() {
+        println!("Score: {}", score.value.to_string());
+    }
+}
+
+pub fn tick_candy_spawn_timer(
+    mut candy_spawn_timer: ResMut<CandySpawnTimer>,
+    time: Res<Time>,
+) {
+    candy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_candies_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    candy_spawn_timer: Res<CandySpawnTimer>,
+) {
+    if candy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/candy.png"),
+                ..default()
+            },
+            Candy {}
+        ));
+    }
+}
+
+pub fn tick_enemy_spawn_timer(
+    mut enemy_spawn_timer: ResMut<EnemySpawnTimer>,
+    time: Res<Time>,
+) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemies_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: Res<EnemySpawnTimer>,
+) {
+    if enemy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/enemy.png"),
+                ..default()
+            },
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            },
+        ));
+    }
+}
+// pub fn change_sprites(
+//     keyboard_input: Res<Input<KeyCode>>,
+//     mut player_query: Query<&mut SpriteBundle, With<Player>>,
+//     asset_server: Res<AssetServer>,
+//     time: Res<Time>,
+// ) {
+//     if let Ok(mut player_sprite) = player_query.get_single_mut() {
+//         player_sprite
+//     }
+// }
